3d4fe1d74cd2f19b1e9e8c4414e281094a118a6b
Jc Briones <madeby@jcbriones.com>
2018-01-26 23:15:24 -0500

    Added base code for new project for iOS

---
+++ iOS/PlanIt - MLH/PlanIt - MLH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlanIt - MLH
+//
+//  Created by John Christopher Briones on 1/26/18.
+//  Copyright © 2018 John Christopher Briones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
